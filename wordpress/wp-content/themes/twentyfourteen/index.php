17e3c43313d99df92636a4fcec71b0b6ab9fefab
baerdaniel <dbaer@Daniel-Bar.local>
2014-08-31 20:22:15 -0400

    Set up taxonomies and custom post types

--- wordpress/wp-content/themes/twentyfourteen/index.php
+++ wordpress/wp-content/themes/twentyfourteen/index.php
@@ -18,15 +18,11 @@ get_header(); ?>
 
 <div id="main-content" class="main-content">
 
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
+
+		<?php query_posts( 'post_type=typefaces'); ?>
 
 		<?php
 			if ( have_posts() ) :
